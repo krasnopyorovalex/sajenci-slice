2d95d38cbad6a80743cd348ec38b098396d226f5
Sanek <djShtaket88@mail.ru>
2019-07-24 12:08:06 +0300

    task #1776: project init

---
+++ gulpfile.babel.js
@@ -0,0 +1,93 @@
+"use strict";
+
+import gulp from "gulp";
+
+const requireDir = require("require-dir"),
+    paths = {
+        views: {
+            src: [
+                "./src/views/index.html",
+                "./src/views/pages/*.html"
+            ],
+            dist: "./dist/",
+            watch: [
+                "./src/blocks/**/*.html",
+                "./src/views/**/*.html"
+            ]
+        },
+        styles: {
+            src: "./src/styles/main.scss",
+            dist: "./dist/styles/",
+            watch: [
+                "./src/blocks/**/*.scss",
+                "./src/styles/**/*.scss"
+            ]
+        },
+        scripts: {
+            src: "./src/js/index.js",
+            dist: "./dist/js/",
+            watch: [
+                "./src/blocks/**/*.js",
+                "./src/js/**/*.js"
+            ]
+        },
+        images: {
+            src: [
+                "./src/img/**/*.{jpg,jpeg,png,gif,tiff,svg}",
+                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
+            ],
+            dist: "./dist/img/",
+            watch: [
+                "./src/img/**/*.{jpg,jpeg,png,gif,svg,tiff}",
+                "!./src/img/icons-sprite/*.{jpg,jpeg,png,tiff}"
+            ]
+        },
+        webp: {
+            src: [
+                "./src/img/**/*.{jpg,jpeg,png,tiff}",
+                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
+            ],
+            dist: "./dist/img/",
+            watch: [
+                "./src/img/**/*.{jpg,jpeg,png,tiff}",
+                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
+            ]
+        },
+        sprites: {
+            src: "./src/img/svg/*.svg",
+            dist: "./dist/img/sprites/",
+            watch: "./src/img/svg/*.svg"
+        },
+        spritesImg: {
+            src: "./src/img/icons-sprite/*.png",
+            dist: "./dist/img/sprites/",
+            watch: "./src/img/icons-sprite/*.png",
+            out: "./src/styles/vendor/import/"
+        },
+        fonts: {
+            src: "./src/fonts/**/*.{woff,woff2}",
+            dist: "./dist/fonts/",
+            watch: "./src/fonts/**/*.{woff,woff2}"
+        },
+        favicons: {
+            src: "./src/img/favicon/*.{jpg,jpeg,png,gif}",
+            dist: "./dist/img/favicons/",
+        },
+        gzip: {
+            src: "./src/.htaccess",
+            dist: "./dist/"
+        }
+    };
+
+requireDir("./gulp-tasks/");
+
+export { paths };
+
+export const development = gulp.series("clean", "smart-grid",
+    gulp.parallel(["views", "styles", "scripts", "images", "webp", "sprites", "sprites-img", "fonts", "favicons"]),
+    gulp.parallel("serve"));
+
+export const prod = gulp.series("clean",
+    gulp.series(["views", "styles", "scripts", "images", "webp", "sprites", "sprites-img", "fonts", "favicons", "gzip"]));
+
+export default development;
